0f12b5565e4405b27eaecd25cd76370898b183c8
Steven Salka <stevensalka925@gmail.com>
2018-04-22 20:25:38 -0400

    add help CLI command w/ description of arguments + examples

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,17 @@
 import program from 'commander';
 import coinvert from './coinvert';
 
+const examples = `
+  Examples:
+
+    coinvert 1 ETH to BTC
+    coinvert 1 BTC to USD
+`;
+
 export default program
   .arguments('<amount> <base> <to> <target>')
+  .usage('<amount> <base> <to> <target>')
+  .on('--help', () => console.log(examples))
   .action((amount, base, to, target) => {
     coinvert(amount, base, target).then(value => {
       console.log(`${amount} ${base} = ${value} ${target}`);
